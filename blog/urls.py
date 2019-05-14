3514f86b8667a85ec6669ecdbb1aa21ce30108f5
NTH <thnam@NTHui-MacBook-Pro.local>
2019-05-14 22:34:40 +0900

    create blog app

---
+++ blog/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, register_converter
+from . import views
+
+app_name = 'blog'
+
+urlpatterns = [
+    
+]
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def archives_year(requests, year):
+    return HttpResponse("{}년도에 대한 내용".format(year))
